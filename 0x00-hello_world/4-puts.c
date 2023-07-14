b6d477c35d8489c42f1ebd93c88fd3acbaf60371
Mohamedswahed <mohamedswahed@gmail.com>
2023-07-13 18:45:15 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main: entry point
+ *
+ *description: prints Using the function puts
+ * 
+ *return: the program return zero
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
