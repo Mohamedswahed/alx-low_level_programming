e2f36c11594be48a6e78e49ae1db2fc23991e395
Mohamedswahed <mohamedswahed@gmail.com>
2023-07-31 13:11:09 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,20 +3,23 @@
 /**
  * _strstr - finds the first occurrence of substring
  * @haystack: string to be scanned
- * @needle: string containing the sequence 
+ * @needle: string containing the sequence
  * Return: pointer to the beginning of the located substring, or NULL
 */
+
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; haystack[i]; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] && haystack[i + j]; j++)
+		for (j = 0; needle[j] != '\0'; j++)
+		{
 			if (haystack[i + j] != needle[j])
 				break;
+		}
 		if (!needle[j])
-			return (haystack + i);
+			return (&haystack[i]);
 	}
 	return (NULL);
 }
