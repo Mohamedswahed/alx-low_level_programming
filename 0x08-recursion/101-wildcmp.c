552d1692dfd003ffc29c2848cfcc650d906474c7
Mohamedswahed <mohamedswahed@gmail.com>
2023-08-02 19:24:46 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * wildcmp - compares two strings and returns 1 
- * 	if the strings are identical otherwise return 0.
+ * wildcmp - compares two strings and returns 1
+ *	if the strings are identical otherwise return 0.
  * @s1:  a string
  * @s2: the string which is  containing "*"
  *
@@ -22,4 +22,4 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
 	return (0);
-}i
+}
