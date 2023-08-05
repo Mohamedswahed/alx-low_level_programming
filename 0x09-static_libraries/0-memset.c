139b8634b9037e8c09965d2bf447221aa80ea6c6
Mohamedswahed <mohamedswahed@gmail.com>
2023-08-04 19:26:32 -0700

    Static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: Returns a pointer to the memory area s
+ * @b: char to copy
+ * @n: number of bytes to to be filled
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+
+	return (s);
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat - function concatenates two srtings
+ * @dest: pointer to a char
+ * @src:  pointer to a char
+ * Return: return value of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i]; i++)
+		;
+	for (j = 0; src[j]; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
