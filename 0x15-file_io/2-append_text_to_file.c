628f1d73e6b2a06405ca4a95e2722bea859a3f4e
Mohamedswahed <mohamedswahed@gmail.com>
2023-09-04 11:36:00 -0700

    append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: filename of the file 
+ * @filename: filename of the file
  * @text_content: the NULL terminated string.
  *
  * Return: 1 on success and -1 on failure.
